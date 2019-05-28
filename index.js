08079d7453fcf769ef6ae07709dacab22ad48e40
geoffrey woodcock <theatomproject@gmail.com>
2019-05-28 20:09:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,6 +4,15 @@ class Driver {
      this.id = ++driverId;
      store.drivers.push(this);
   }
+   trips(){
+    return store.trips.filter(trip => trip.driverId === this.id);
+  }
+
+
+  passengers(){
+    let trips = this.trips();
+    return trips.map(trip => trip.passenger());
+  }
 }
 
 
